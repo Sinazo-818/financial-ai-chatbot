2c26ea41dc38e82fe0bc387592cf1e9058658fb2
Sinazo-818 <dyubelesinazo@outlook.com>
2026-03-24 12:53:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -21,28 +21,28 @@ user_input = st.text_input("Enter your question:")
 
 def ask_llm(question):
     try:
-    prompt = f"""
-    You are a financial analyst.
+        prompt = f"""
+        You are a financial analyst.
 
-    Use ONLY the data below to answer.
+        Use ONLY the data below to answer.
 
-    If the question is about the future, say:
-    "I do not have enough information to answe future-oriented questions."
+        If the question is about the future, say:
+       "I do not have enough information to answe future-oriented questions."
 
-    DATA
-    {financial_context}
+        DATA
+        {financial_context}
 
-    QUESTION
-    {question}
-    """
+        QUESTION
+        {question}
+        """
 
-    response = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[{"role": "user", "content": prompt}],
-        temperature=0.2
-    )
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[{"role": "user", "content": prompt}],
+            temperature=0.2
+        )
 
-    return response.choices[0].message.content
+        return response.choices[0].message.content
 
 except Exception:
         return "⚠️ API issue or limit reached. Please try again later."
